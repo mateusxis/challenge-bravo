4bea548d8f328ac2a520fff49798589383450de5
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-09-10 09:49:58 -0300

    [container] add redis

--- src/container.js
+++ src/container.js
@@ -2,11 +2,13 @@ const { createContainer, asFunction } = require('awilix');
 
 const router = require('./application/routes');
 const server = require('./application/server');
+const redisFactory = require('./infrastructure/redis');
 const configEnvs = require('./config');
 
 const container = createContainer();
 
 container.register({
+  redis: asFunction(redisFactory).singleton(),
   router: asFunction(router).singleton(),
   server: asFunction(server).singleton(),
   config: asFunction(configEnvs).singleton()
