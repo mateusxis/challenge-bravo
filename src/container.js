ab85658729328d28904cab47ffffb670f77a741f
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-09-10 09:45:54 -0300

    [container] add config

--- src/container.js
+++ src/container.js
@@ -2,12 +2,14 @@ const { createContainer, asFunction } = require('awilix');
 
 const router = require('./application/routes');
 const server = require('./application/server');
+const configEnvs = require('./config');
 
 const container = createContainer();
 
 container.register({
   router: asFunction(router).singleton(),
-  server: asFunction(server).singleton()
+  server: asFunction(server).singleton(),
+  config: asFunction(configEnvs).singleton()
 });
 
 module.exports = container;
