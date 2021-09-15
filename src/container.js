3a5895eb35fc58f56457e2491708a47ddd12e4b4
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-09-15 14:07:09 -0300

    [container] add database

--- src/container.js
+++ src/container.js
@@ -2,12 +2,14 @@ const { createContainer, asFunction } = require('awilix');
 
 const router = require('./application/routes');
 const server = require('./application/server');
+const databaseFactory = require('./infrastructure/database');
 const redisFactory = require('./infrastructure/redis');
 const configEnvs = require('./config');
 
 const container = createContainer();
 
 container.register({
+  database: asFunction(databaseFactory).singleton(),
   redis: asFunction(redisFactory).singleton(),
   router: asFunction(router).singleton(),
   server: asFunction(server).singleton(),
