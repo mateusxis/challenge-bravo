aeaf13c2705314b6a881226b76e75ee9a6b6642b
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-09-15 14:07:48 -0300

    [routes] update endpoint readiness

--- src/application/routes.js
+++ src/application/routes.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router');
+const { OK, INTERNAL_SERVER_ERROR } = require('http-status');
 
-module.exports = () => {
+module.exports = ({ database, redis }) => {
   const router = new Router();
 
   router.get('/liveness', async (ctx) => {
@@ -8,7 +9,13 @@ module.exports = () => {
   });
 
   router.get('/readiness', async (ctx) => {
-    ctx.body = 'OK';
+    const mongoLiveness = database.connection.readyState === 1;
+    const redisLiveness = redis.isUp();
+
+    const readiness = redisLiveness && mongoLiveness;
+
+    ctx.status = readiness ? OK : INTERNAL_SERVER_ERROR;
+    ctx.body = readiness ? 'OK' : 'NOK';
   });
 
   return router;
