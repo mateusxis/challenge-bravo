fc6007dcdba3b31e05e28ec6c974d89a244dca0a
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-09-16 09:55:31 -0300

    [container] add currency repository and user repository

--- src/container.js
+++ src/container.js
@@ -4,16 +4,20 @@ const router = require('./application/routes');
 const server = require('./application/server');
 const databaseFactory = require('./infrastructure/database');
 const redisFactory = require('./infrastructure/redis');
+const currencyRepository = require('./infrastructure/repositories/currency');
+const userRepository = require('./infrastructure/repositories/user');
 const configEnvs = require('./config');
 
 const container = createContainer();
 
 container.register({
+  config: asFunction(configEnvs).singleton(),
+  currencyRepository: asFunction(currencyRepository).singleton(),
+  userRepository: asFunction(userRepository).singleton(),
   database: asFunction(databaseFactory).singleton(),
   redis: asFunction(redisFactory).singleton(),
   router: asFunction(router).singleton(),
-  server: asFunction(server).singleton(),
-  config: asFunction(configEnvs).singleton()
+  server: asFunction(server).singleton()
 });
 
 module.exports = container;
